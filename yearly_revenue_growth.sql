6f2077d2b452c1d67925ecf9ce783df9bf914c86
dagash007 <37184758+dagash007@users.noreply.github.com>
2021-10-27 12:16:12 +0100

    Add files via upload

--- yearly_revenue_growth.sql
+++ yearly_revenue_growth.sql
@@ -5,4 +5,6 @@ select * from dbo.['2019$']
 union
 select * from dbo.['2020$']
 )
-select (stays_in_week_nights + stays_in_weekend_nights)*adr as revenue from hotels
+select arrival_date_year,hotel,
+round(sum((stays_in_week_nights + stays_in_weekend_nights)*adr),2) as revenue from hotels
+group by arrival_date_year,hotel
